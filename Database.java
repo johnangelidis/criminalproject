e5d5f12e5a317b49b72ebe5ed4a7d330703f4f45
Coby Arambula <cobyarambula@gmail.com>
2021-03-10 18:57:22 -0500

    Database class stub

---
+++ Database.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Database {
+    private ArrayList<Criminal> criminals;
+    private ArrayList<Civilian> civilians;
+    private ArrayList<PoliceOfficer> officers;
+    private ArrayList<Detectives> detectives;
+    private ArrayList<Case> cases;
+    private ArrayList<Gang> gangs;
+    private ArrayList<User> users;
+
+    public Person SearchPerson(int id, String firstName, String lastName)
+    {
+
+    }
+
+    public Gang SearchGang(String name)
+    {
+
+    }
+
+    public Case SearchCase(int id)
+    {
+
+    }
+
+    public void exportPerson(int id)
+    {
+
+    }
+
+    public void exportGang(String name)
+    {
+
+    }
+
+    public void exportCase(int id)
+    {
+
+    }
+
+    public String toString()
+    {
+        
+    }
+}
