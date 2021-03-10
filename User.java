8ac1796245174a4c18bdf1dddc9cc10e5d3d9fe5
Coby Arambula <cobyarambula@gmail.com>
2021-03-10 18:49:52 -0500

    User class stub

---
+++ User.java
@@ -0,0 +1,14 @@
+
+public class User {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean isAdmin;
+    // May need to add isLoggedIn boolean
+
+    public void login(){}
+
+    public void logout(){}
+    
+}
