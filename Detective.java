63a2d7861bd205c1b0441c14b66939b148d408de
Coby Arambula <cobyarambula@gmail.com>
2021-03-10 19:39:17 -0500

    Added JavaDoc for Detective class stub

--- Detective.java
+++ Detective.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
 
+/**
+ * Class representation of a Detective
+ */
 public class Detective {
     private ArrayList<Case> cases;
     
+    /**
+     * Parameterized constructor for Dectective class
+     * @param cases ArrayList of cases Dectective has been associated with
+     */
     public Detective(ArrayList<Case> cases)
     {
         this.cases = cases;
