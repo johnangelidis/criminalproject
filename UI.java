346d9b856a4354c6fc9c932875ae775651ce8720
Coby Arambula <cobyarambula@gmail.com>
2021-03-11 10:41:51 -0500

    added UI class

---
+++ UI.java
@@ -0,0 +1,7 @@
+public class UI {
+    public static void main(String[] args) {
+        Tattoo tatoo = new Tattoo("asgd", "blue", "adgad");
+        System.out.println("hello world");
+        System.out.println(tatoo);
+    }
+}
