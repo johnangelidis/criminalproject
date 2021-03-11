8996174712b6a07967a63d2adb770bfee5e0ed24
Coby Arambula <cobyarambula@gmail.com>
2021-03-10 19:16:49 -0500

    Dectective class stub

---
+++ Detective.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Detective {
+    private ArrayList<Case> cases;
+    
+    public Detective(ArrayList<Case> cases)
+    {
+        this.cases = cases;
+    }
+}
