97c3eff0768787a8b69753d962ef01b05758a83b
challison404 <challison404@users.noreply.github.com>
2021-03-10 20:10:17 -0500

    Created Gang Class

---
+++ Gang.java
@@ -0,0 +1,64 @@
+/**
+ * A class to represent the Gangs in the system
+ * @author challison404
+ *
+ */
+import java.util.ArrayList;
+public class Gang {
+	private String name;
+	private ArrayList<Criminals> members;
+	private ArrayList<Cases> crimes;
+	/**
+	 * A constructor to create new gangs
+	 * @param name The name of the gang
+	 * @param members Who is affiliated with the gang
+	 * @param crimes What crimes the gang has commited
+	 */
+	public Gang(String name, ArrayList<Criminals> members, ArrayList<Cases> crimes) {
+		this.name = name;
+		this.members = members;
+		this.crimes = crimes;
+	}
+	/**
+	 * Get name of the gang
+	 * @return The name of the gang
+	 */
+	public String getName() {
+		return this.name;
+	}
+	/**
+	 * Get gangmembers of selected gang
+	 * @return the arraylist of criminals affiliated with the gang
+	 */
+	public ArrayList<Criminals> getMembers() {
+		return this.members;
+	}
+	/**
+	 * Get crimes gang is responsible for
+	 * @return the arraylist of cases involving the gang
+	 */
+	public ArrayList<Cases> getCrimes() {
+		return this.crimes;
+	}
+	/**
+	 * Set name of the gang
+	 * @param aName the name the gang will be set to
+	 */
+	public void setName(String aName) {
+		this.name = aName;
+	}
+	/**
+	 * Set affiliated members of the gang
+	 * @param aMembers ArrayList of Criminals to be updated containing new members of the gang
+	 */
+	public void setMembers(ArrayList<Criminals> aMembers) {
+		this.members = aMembers;
+	}
+	/**
+	 * Set cases the gang is responsible for
+	 * @param aCrimes ArrayList of Cases to be updated containing current cases the gang is responsible for
+	 */
+	public void setCrimes(ArrayList<Cases> aCrimes) {
+		this.crimes = aCrimes;
+	}
+}
