c6eff1f6fa21fa618dccd798b9eea3fdd2c513e0
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-13 18:10:55 -0500

    Completed class

--- Detective.java
+++ Detective.java
@@ -3,15 +3,33 @@ import java.util.ArrayList;
 /**
  * Class representation of a Detective
  */
-public class Detective {
+public class Detective extends Person{
     private ArrayList<Case> cases;
     
     /**
      * Parameterized constructor for Dectective class
      * @param cases ArrayList of cases Dectective has been associated with
      */
-    public Detective(ArrayList<Case> cases)
+    public Detective(int id, String firstName, String lastName, ArrayList<Case> cases)
     {
+        super(id, firstName, lastName);
         this.cases = cases;
+    }
+
+    /**
+     * Method that prints the id of every case the detective has worked on
+     */
+    public void getCases(){
+        for(Case c:cases){
+            System.out.print(c.getId() + " ");
+        }
+    }
+    /**
+     * Method that returns a string with the detective's first and last name
+     * @return a string with the detective's first and last name
+     */
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " is a detective";
     }
 }
