8eb090d93f772251c6ae79c236a285e1240a4c2a
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-13 19:38:19 -0500

    Added getters and setters

--- User.java
+++ User.java
@@ -6,8 +6,48 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     private boolean isAdmin;
     // May need to add isLoggedIn boolean
+
+
 
     /**
      * Logs the user into the database
