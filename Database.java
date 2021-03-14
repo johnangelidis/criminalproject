752cb62cf5a33953d822efa35054c92987a0d316
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-14 13:04:22 -0400

    Complete

--- Database.java
+++ Database.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
 /**
  * Class representation of Database that allows user to search and export entries
  */
@@ -7,21 +9,40 @@ public class Database {
     private ArrayList<Criminal> criminals;
     private ArrayList<Civilian> civilians;
     private ArrayList<PoliceOfficer> officers;
-    private ArrayList<Detectives> detectives;
+    private ArrayList<Detective> detectives;
     private ArrayList<Case> cases;
     private ArrayList<Gang> gangs;
     private ArrayList<User> users;
 
     /**
      * Allows user to search entries based on people associated with those entries
-     * @param id ID number associated with person entry
      * @param firstName First name of person
      * @param lastName Last name of person
      * @return instance of the Person class
      */
-    public Person SearchPerson(int id, String firstName, String lastName)
+    public Person searchPerson(String firstName, String lastName)
     {
-
+        for (Criminal c : criminals) {
+            if (c.getFirstName().equals(firstName) && c.getLastName().equals(lastName)) {
+                return c;
+            }
+        }
+        for (Civilian c : civilians) {
+            if (c.getFirstName().equals(firstName) && c.getLastName().equals(lastName)) {
+                return c;
+            }
+        }
+        for (PoliceOfficer p : officers) {
+            if (p.getFirstName().equals(firstName) && p.getLastName().equals(lastName)) {
+                return p;
+            }
+        }
+        for (Detective d : detectives) {
+            if (d.getFirstName().equals(firstName) && d.getLastName().equals(lastName)) {
+                return d;
+            }
+        }
+        return null;
     }
 
     /**
@@ -29,9 +50,14 @@ public class Database {
      * @param name name of the gang
      * @return instance of the Gang class
      */
-    public Gang SearchGang(String name)
+    public Gang searchGang(String name)
     {
-
+        for(Gang g:gangs){
+            if(g.getName().equals(name)){
+                return g;
+            }
+        }
+        return null;
     }
 
     /**
@@ -39,43 +65,72 @@ public class Database {
      * @param id ID number associated with case
      * @return instance of the Case class
      */
-    public Case SearchCase(int id)
+    public Case searchCase(int id)
     {
+        for(Case c:cases){
+            if(c.getId()==id){
+                return c;
+            }
+        }
+        return null;
+    }
 
+    public User searchUser(String username){
+        for(User u:users){
+            if(u.getUsername().equals(username)){
+                return u;
+            }
+        }
+        return null;
     }
 
     /**
      * Allows user to export a Person entry
-     * @param id Id number associated with person entry
+     * @param p Person that we want to write to file
      */
-    public void exportPerson(int id)
-    {
-
+    public void exportPerson(Person p){
+        try{
+            FileOutputStream output = new FileOutputStream("c:\\Users\\J\\Desktop");
+            ObjectOutputStream objectOut = new ObjectOutputStream(output);
+            objectOut.writeObject(p);
+            System.out.println("Person successfully written to file!");
+            objectOut.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     /**
      * Allows user to export a Gang entry
-     * @param name String representation of the Gang's name
+     * @param g Gang that we want to write to file
      */
-    public void exportGang(String name)
+    public void exportGang(Gang g)
     {
-
+        try{
+            FileOutputStream output = new FileOutputStream("c:\\Users\\J\\Desktop");
+            ObjectOutputStream objectOut = new ObjectOutputStream(output);
+            objectOut.writeObject(g);
+            System.out.println("Gang successfully written to file!");
+            objectOut.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     /**
      * Allows user to export a Case entry
-     * @param id ID number associated with the Case
+     * @param c Case that we want to write to file
      */
-    public void exportCase(int id)
+    public void exportCase(Case c)
     {
-
-    }
-
-    /**
-     * @return String representation of what method was used by the user
-     */
-    public String toString()
-    {
-
+        try{
+            FileOutputStream output = new FileOutputStream("c:\\Users\\J\\Desktop");
+            ObjectOutputStream objectOut = new ObjectOutputStream(output);
+            objectOut.writeObject(c);
+            System.out.println("Case successfully written to file!");
+            objectOut.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
