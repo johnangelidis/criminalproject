7853e040f6809267bcf4d6b7761028907f11e4e2
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-15 18:17:58 -0400

    Added classes that process Civilian JSON

---
+++ CivilianConstants.java
@@ -0,0 +1,16 @@
+public abstract class CivilianConstants {
+	protected static final String CIVILIANS_FILE_NAME = "json/civilians.json";
+	protected static final String CIVILIANS = "civilians";
+	protected static final String CIVILIAN_ID = "id";
+	protected static final String CIVILIAN_FIRST_NAME = "firstName";
+	protected static final String CIVILIAN_LAST_NAME = "lastName";
+	protected static final String CIVILIAN_DOB = "dateOfBirth";
+	protected static final String CIVILIAN_ADDRESS = "address";
+	protected static final String CIVILIAN_PHONE_NUMBER = "phoneNumber";
+	protected static final String CIVILIAN_IS_VICTIM = "isVictim";
+	protected static final String CIVILIAN_IS_WITNESS = "isWitness";
+	protected static final String CIVILIAN_IS_PERSON_OF_INTEREST = "isPersonOfInterest";
+	protected static final String CIVILIAN_CASES = "cases";
+
+
+}
---
+++ CivilianWriter.java
@@ -0,0 +1,46 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class CivilianWriter extends CivilianConstants {
+	
+	
+	public static void saveCivilians() {
+		Database db = Database.getInstance();
+		ArrayList<Civilian> civilians = db.getCivilians();
+		JSONArray jsonCivilians = new JSONArray();
+		
+		//creating all the json objects
+		for(int i=0; i< civilians.size(); i++) {
+			jsonCivilians.add(getCivilianJSON(civilians.get(i)));
+		}
+		
+		//Write JSON file
+        try (FileWriter file = new FileWriter(CIVILIANS_FILE_NAME)) {
+ 
+            file.write(jsonCivilians.toJSONString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static JSONObject getCivilianJSON(Civilian c) {
+		JSONObject civilianDetails = new JSONObject();
+        //civilianDetails.put(CIVILIAN_ID), c.getId());
+		civilianDetails.put(CIVILIAN_FIRST_NAME, c.getFirstName());
+		civilianDetails.put(CIVILIAN_LAST_NAME, c.getLastName());
+        civilianDetails.put(CIVILIAN_DOB, c.getDOB());
+        civilianDetails.put(CIVILIAN_ADDRESS, c.getAddress());
+		civilianDetails.put(CIVILIAN_PHONE_NUMBER, c.getPhone());
+        civilianDetails.put(CIVILIAN_IS_VICTIM, c.getisVictim());
+        civilianDetails.put(CIVILIAN_IS_WITNESS, c.getisWitNess());
+        civilianDetails.put(CIVILIAN_IS_PERSON_OF_INTEREST, c.getisPersonOfInterest());
+        civilianDetails.put(CIVILIAN_CASES, c.getCases());
+        
+        return civilianDetails;
+	}
+}
