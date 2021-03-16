3090eede59076da26a0b4149d79321824fc647f6
Jayxden <77359383+Jayxden@users.noreply.github.com>
2021-03-16 10:19:21 -0400

    Setting up to read in Police JSON

--- PoliceConstants.java
+++ PoliceConstants.java
@@ -1,3 +1,11 @@
 public abstract class PoliceConstants {
+    protected static final String OFFICERS_FILE_NAME = "json/officers.json";
+    protected static final String OFFICERS = "officers";
+    protected static final String OFFICERS_ID = "id";
+    protected static final String OFFICERS_FIRST_NAME = "firtName";
+    protected static final String OFFICERS_LAST_NAME = "lastName";
+    protected static final String OFFICERS_DOB  = "dateOfBirth";
+    protected static final String OFFICERS_DEPT = "policeDept";
+    protected static final String OFFICERS_CASES = "cases"
     
 }
