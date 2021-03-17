85975f65d82cb6a855e1f66587c6573eff4451dc
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-17 15:57:15 -0400

    added user constants for json

---
+++ UserConstants.java
@@ -0,0 +1,9 @@
+public class UserConstants {
+    protected static final String USER_FILE_NAME = "json/users.json";
+	protected static final String USERS = "users";
+	protected static final String USER_ID = "id";
+	protected static final String USER_USERNAME = "username";
+	protected static final String USER_PASSWORD = "password";
+	protected static final String USER_IS_ADMIN = "isAdmin";  
+    protected static final String USER_IS_LOGGED_IN = "isLoggedIn";
+}
