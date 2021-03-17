bf056f6ef93b395f944167e05c95e80ca32fc378
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-17 16:08:01 -0400

    Created user writer for json

---
+++ UserWriter.java
@@ -0,0 +1,40 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class UserWriter extends UserConstants {
+	
+	
+	public static void saveUsers() {
+		Database db = Database.getInstance();
+		ArrayList<User> users = db.getUsers();
+		JSONArray jsonUsers = new JSONArray();
+		
+		//creating all the json objects
+		for(int i=0; i< users.size(); i++) {
+			jsonUsers.add(getUserJSON(users.get(i)));
+		}
+		
+		//Write JSON file
+        try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+ 
+            file.write(jsonUsers.toJSONString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static JSONObject getUserJSON(User user) {
+		JSONObject userDetails = new JSONObject();
+        userDetails.put(USER_ID, user.getId());
+        userDetails.put(USER_USERNAME, user.getUsername());
+        userDetails.put(USER_PASSWORD, user.getPassword());
+        userDetails.put(USER_IS_ADMIN, user.isIsAdmin());
+        
+        return userDetails;
+	}
+}
