482116b07f1de65ea1e9ac95070d2552a492cb8d
challison404 <challison404@users.noreply.github.com>
2021-03-17 21:47:16 -0400

    Created and set up foundation

--- UI.java
+++ UI.java
@@ -1,9 +1,27 @@
+/**
+* Written by Chase Allison
+  The UI which will prompt the user and allow them
+  to make choices and overall use the application
+*/
 import java.util.ArrayList;
+import java.util.Scanner;
+public class UI extends UIConstants{
+    private Scanner scanner;
+    private Database database;
+    //private static ArrayList<PoliceOfficer> loadOfficers;
+    UI(){
+      scanner = new Scanner(System.in);
+      database = new Database();
+    }
+    public void run() {
+      System.out.println(WELCOME_MESSAGE);
+      //Loop until user quits
+      while(true) {
 
-public class UI {
-    private static ArrayList<PoliceOfficer> loadOfficers;
-
+      }
+    }
     public static void main(String[] args) {
-        loadOfficers = PoliceLoader.loadOfficers();
+        UI UIInterface = new UI();
+        UIInterface.run();
     }
 }
