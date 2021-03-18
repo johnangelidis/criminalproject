160c4394fe4eca898cdf62e0e69cf9af0ca01aaf
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-18 02:14:47 -0400

    Created Criminal Constants for JSON

---
+++ CriminalConstants.java
@@ -0,0 +1,15 @@
+public class CriminalConstants {
+	protected static final String CRIMINALS_FILE_NAME = "json/criminals.json";
+	protected static final String CRIMINALS = "criminals";
+	protected static final String CRIMINAL_ID = "id";
+	protected static final String CRIMINAL_FIRST_NAME = "firstName";
+	protected static final String CRIMINAL_LAST_NAME = "lastName";
+	protected static final String CRIMINAL_WEIGHT = "weight";
+	protected static final String CRIMINAL_HEIGHT = "height";
+	protected static final String CRIMINAL_HAIR_COLOR = "hairColor";
+	protected static final String CRIMINAL_EYE_COLOR = "eyeColor";
+	protected static final String CRIMINAL_TATTOOS = "tattoos";
+	protected static final String CRIMINAL_HAS_SCARS = "hasScars";
+	protected static final String CRIMINAL_STATUS = "status";   
+
+}
