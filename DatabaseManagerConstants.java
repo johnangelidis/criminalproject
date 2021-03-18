b3a8c10946cd94bc041019bc1b3f76c24add11b2
Coby Arambula <cobyarambula@gmail.com>
2021-03-18 08:49:42 -0400

    added DatabaseManagerConstants

---
+++ DatabaseManagerConstants.java
@@ -0,0 +1,6 @@
+public abstract class DatabaseManagerConstants {
+    
+    protected static final String DM_FILE_NAME = "json/databasemanager.json";
+    protected static final String DATABASEMANAGER = "database manager";
+    protected static final String USERNAME = "username";
+}
