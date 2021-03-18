bef00795ab4ee1ee32631aede5f14390438cddc0
challison404 <challison404@users.noreply.github.com>
2021-03-17 22:19:44 -0400

    Added mainMenuOptions

--- UIConstants.java
+++ UIConstants.java
@@ -1,3 +1,4 @@
 public abstract class UIConstants {
   protected final static String WELCOME_MESSAGE = "Welcome to the A-Team's crime database application.";
+  protected final static String[] mainMenuOptions = {"Create Account","Login","Quit"};
 }
