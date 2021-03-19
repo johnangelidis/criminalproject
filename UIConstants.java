2612ed812b4a619e8c9ac037497e233976bdb826
challison404 <challison404@users.noreply.github.com>
2021-03-18 20:25:17 -0400

    Further updates

--- UIConstants.java
+++ UIConstants.java
@@ -1,4 +1,4 @@
 public abstract class UIConstants {
   protected final static String WELCOME_MESSAGE = "Welcome to the A-Team's crime database application.";
-  protected final static String[] mainMenuOptions = {"Login","Quit"};
+  protected final static String[] mainMenuOptions = {"Create Account","Login","Quit"};
 }
