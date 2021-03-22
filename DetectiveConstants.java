aea52cdc852ee62349556497c0d29a8b133d733c
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-22 16:51:42 -0400

    removed unnecessary attributes

--- DetectiveConstants.java
+++ DetectiveConstants.java
@@ -5,5 +5,4 @@ public abstract class DetectiveConstants {
     protected static final String DETECTIVE_FIRST_NAME  = "firstName";
     protected static final String DETECTIVE_LAST_NAME = "lastName";
     protected static final String DETECTIVE_DOB = "dateOfBirth";
-    protected static final String DETECTIVE_CASES = "cases";
 }
