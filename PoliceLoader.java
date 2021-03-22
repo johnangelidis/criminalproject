faa2a87dcd30e702c0d88e141f83597a56dd24f7
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-22 17:31:10 -0400

    made some changes

--- PoliceLoader.java
+++ PoliceLoader.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
+import java.util.UUID;
 public class PoliceLoader extends PoliceConstants {
     
     public static ArrayList<PoliceOfficer> loadOfficers() {
@@ -16,15 +16,15 @@ public class PoliceLoader extends PoliceConstants {
 
             for(int i=0; i < officersJSON.size(); i++) {
                 JSONObject officerJSON = (JSONObject)officersJSON.get(i);
-                int id = (Integer)officerJSON.get(OFFICERS_ID);
+                UUID id = (UUID)officerJSON.get(OFFICERS_ID);
                 String firstName = (String)officerJSON.get(OFFICERS_FIRST_NAME);
                 String lastName = (String)officerJSON.get(OFFICERS_LAST_NAME);
                 String dateOfBirth = (String)officerJSON.get(OFFICERS_DOB);
                 int badgeNum = (Integer)officerJSON.get(OFFICERS_BADGENUM);
                 String poliiceDept = (String)officerJSON.get(OFFICERS_DEPT);
-                ArrayList<Case> cases = (ArrayList<Case>)officerJSON.get(OFFICERS_CASES);
+                //ArrayList<Case> cases = (ArrayList<Case>)officerJSON.get(OFFICERS_CASES);
                 
-                officers.add(new PoliceOfficer(id, firstName, lastName, dateOfBirth, badgeNum, poliiceDept, cases));
+                officers.add(new PoliceOfficer(firstName, lastName, dateOfBirth, badgeNum, poliiceDept));
 
             }
             return officers;
