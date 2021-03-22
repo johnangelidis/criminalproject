d6b869c106116460bdcb4f3f431f8e4f8b8f2898
CobyArambula <69974538+CobyArambula@users.noreply.github.com>
2021-03-22 19:53:11 -0400

    Delete DatabaseManagerConstants.java

--- DatabaseManagerConstants.java
+++
@@ -1,5 +0,0 @@
-public abstract class DatabaseManagerConstants {
-    
-    protected static final String DM_FILE_NAME = "json/databasemanager.json";
-    protected static final User USERARRAYLIST = new User();
-}
