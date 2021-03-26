520c9903096d5d5713aa73f1fc24e2f319c001d3
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-03-26 12:07:41 -0400

    changed attributes

--- Criminal.java
+++ Criminal.java
@@ -2,39 +2,37 @@
  * Class representation of a Criminal
  */
 public class Criminal extends Person{
+    private String race;
     private double weight;
     private double height;
-    private String hairColor;
     private String eyeColor;
-    private Tattoo tattoos;
-    private boolean hasScars;
+    private String hairColor;
+    private String hairLength;
+    private String facialHair;
+    private Tattoo tattoo;
     private String status;
-    private boolean isSerialCriminal;
+    private Gang gang;
 
-    /**
-     * Parameterized constructor for Criminal class
-     * @param weight double representation of criminal's weight
-     * @param height double representation of criminal's height
-     * @param hairColor String representation of criminal's hair color
-     * @param eyeColor String representation of criminal's eye color
-     * @param tattoos Instance of Tattoos class that represent criminal's tattoos
-     * @param hasScars Boolean value that indicates if criminal has scars or not
-     * @param status String representation of criminal's incarceration status
-     * @param gang Instance of Gang class that represents the criminal's gang association
-     * @param isSerialCriminal Boolean value that indicates if criminal is a serial criminal or not
-     */
-    public Criminal(String firstName, String lastName, Date dateOfBirth, double weight, double height, String hairColor,
-                    String eyeColor, Tattoo tattoos, boolean hasScars,
-                    String status, boolean isSerialCriminal) {
+    public Criminal(String firstName, String lastName, Date dateOfBirth, String race, double weight, double height, String eyeColor, String hairColor, String hairLength, String facialHair, Tattoo tattoo, String status, Gang gang) {
         super(firstName, lastName, dateOfBirth);
-        setWeight(weight);
-        setHeight(height);
-        setHairColor(hairColor);
-        setEyeColor(eyeColor);
-        setTattoos(tattoos);
-        setHasScars(hasScars);
-        setStatus(status);
-        setIsSerialCriminal(isSerialCriminal);
+        this.race = race;
+        this.weight = weight;
+        this.height = height;
+        this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
+        this.hairLength = hairLength;
+        this.facialHair = facialHair;
+        this.tattoo = tattoo;
+        this.status = status;
+        this.gang = gang;
+    }
+
+    public String getRace() {
+        return this.race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
     }
 
     public double getWeight() {
@@ -53,14 +51,6 @@ public class Criminal extends Person{
         this.height = height;
     }
 
-    public String getHairColor() {
-        return this.hairColor;
-    }
-
-    public void setHairColor(String hairColor) {
-        this.hairColor = hairColor;
-    }
-
     public String getEyeColor() {
         return this.eyeColor;
     }
@@ -69,20 +59,36 @@ public class Criminal extends Person{
         this.eyeColor = eyeColor;
     }
 
-    public Tattoo getTattoos() {
-        return this.tattoos;
+    public String getHairColor() {
+        return this.hairColor;
     }
 
-    public void setTattoos(Tattoo tattoos) {
-        this.tattoos = tattoos;
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
     }
 
-    public boolean isHasScars() {
-        return this.hasScars;
+    public String getHairLength() {
+        return this.hairLength;
     }
 
-    public void setHasScars(boolean hasScars) {
-        this.hasScars = hasScars;
+    public void setHairLength(String hairLength) {
+        this.hairLength = hairLength;
+    }
+
+    public String getFacialHair() {
+        return this.facialHair;
+    }
+
+    public void setFacialHair(String facialHair) {
+        this.facialHair = facialHair;
+    }
+
+    public Tattoo getTattoo() {
+        return this.tattoo;
+    }
+
+    public void setTattoo(Tattoo tattoo) {
+        this.tattoo = tattoo;
     }
 
     public String getStatus() {
@@ -93,13 +99,15 @@ public class Criminal extends Person{
         this.status = status;
     }
 
-    public boolean isIsSerialCriminal() {
-        return this.isSerialCriminal;
+    public Gang getGang() {
+        return this.gang;
     }
 
-    public void setIsSerialCriminal(boolean isSerialCriminal) {
-        this.isSerialCriminal = isSerialCriminal;
+    public void setGang(Gang gang) {
+        this.gang = gang;
     }
+
+
 
     /**
      * @return String representation of criminal's first and last name
